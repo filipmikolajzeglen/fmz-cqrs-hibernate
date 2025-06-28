651b34f4325448477f5c15481bb771da883f8b34
Filip Żegleń <filipmikolajzeglen@gmail.com>
2025-06-28 19:49:49 +0200

    FMZ-CQRS-002 Refactor DatabaseCommand and DatabaseCommandHandler to use execute method for command handling

--- src/main/java/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseCommand.java
+++ src/main/java/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseCommand.java
@@ -1,53 +1,90 @@
 package com.filipmikolajzeglen.cqrs.hibernate.database;
 
 import com.filipmikolajzeglen.cqrs.common.Command;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.EntityManager;
 
 public abstract class DatabaseCommand<ENTITY> extends Command<ENTITY>
 {
-   public static <ENTITY> Create<ENTITY> create(ENTITY entity)
+   public abstract ENTITY execute(EntityManager entityManager);
+
+   public static <ENTITY> DatabaseCommand<ENTITY> create(ENTITY entity)
    {
       return new Create<>(entity);
    }
 
-   public static <ENTITY> Update<ENTITY> update(ENTITY entity)
+   public static class Create<ENTITY> extends DatabaseCommand<ENTITY>
+   {
+      private final ENTITY entity;
+
+      public Create(ENTITY entity)
+      {
+         this.entity = entity;
+      }
+
+      @Override
+      public ENTITY execute(EntityManager entityManager)
+      {
+         entityManager.persist(entity);
+         return entity;
+      }
+   }
+
+   public static <ENTITY> DatabaseCommand<ENTITY> update(ENTITY entity)
    {
       return new Update<>(entity);
    }
 
-   public static <ENTITY> Remove<ENTITY> remove(ENTITY entity)
+   public static class Update<ENTITY> extends DatabaseCommand<ENTITY>
+   {
+      private final ENTITY entity;
+
+      public Update(ENTITY entity)
+      {
+         this.entity = entity;
+      }
+
+      @Override
+      public ENTITY execute(EntityManager entityManager)
+      {
+         return entityManager.merge(entity);
+      }
+   }
+
+   public static <ENTITY> DatabaseCommand<ENTITY> remove(ENTITY entity)
    {
       return new Remove<>(entity);
    }
 
-   public static <ENTITY> Flush<ENTITY> flush()
+   public static class Remove<ENTITY> extends DatabaseCommand<ENTITY>
+   {
+      private final ENTITY entity;
+
+      public Remove(ENTITY entity)
+      {
+         this.entity = entity;
+      }
+
+      @Override
+      public ENTITY execute(EntityManager entityManager)
+      {
+         ENTITY managed = entityManager.merge(entity);
+         entityManager.remove(managed);
+         return managed;
+      }
+   }
+
+   public static <ENTITY> DatabaseCommand<ENTITY> flush()
    {
       return new Flush<>();
    }
 
-   @Getter
-   @RequiredArgsConstructor
-   public static class Create<ENTITY> extends DatabaseCommand<ENTITY>
-   {
-      private final ENTITY entity;
-   }
-
-   @Getter
-   @RequiredArgsConstructor
-   public static class Update<ENTITY> extends DatabaseCommand<ENTITY>
-   {
-      private final ENTITY entity;
-   }
-
-   @Getter
-   @RequiredArgsConstructor
-   public static class Remove<ENTITY> extends DatabaseCommand<ENTITY>
-   {
-      private final ENTITY entity;
-   }
-
    public static class Flush<ENTITY> extends DatabaseCommand<ENTITY>
    {
+      @Override
+      public ENTITY execute(EntityManager entityManager)
+      {
+         entityManager.flush();
+         return null;
+      }
    }
 }
--- src/main/java/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseCommandHandler.java
+++ src/main/java/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseCommandHandler.java
@@ -14,29 +14,6 @@ public class DatabaseCommandHandler<ENTITY> implements CommandHandler<DatabaseCo
    @Override
    public ENTITY handle(DatabaseCommand<ENTITY> command)
    {
-      switch (command)
-      {
-         case DatabaseCommand.Create<ENTITY> create ->
-         {
-            entityManager.persist(create.getEntity());
-            return create.getEntity();
-         }
-         case DatabaseCommand.Update<ENTITY> update ->
-         {
-            return entityManager.merge(update.getEntity());
-         }
-         case DatabaseCommand.Remove<ENTITY> remove ->
-         {
-            ENTITY managed = entityManager.merge(remove.getEntity());
-            entityManager.remove(managed);
-            return managed;
-         }
-         case DatabaseCommand.Flush<ENTITY> entityFlush ->
-         {
-            entityManager.flush();
-            return null;
-         }
-         default -> throw new IllegalArgumentException("Unknown DatabaseCommand: " + command.getClass());
-      }
+      return command.execute(entityManager);
    }
 }
