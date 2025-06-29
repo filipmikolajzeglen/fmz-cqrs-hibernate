6a2d9b28f31594ddacf2b102570ce70bee523fb9
Filip Żegleń <filipmikolajzeglen@gmail.com>
2025-06-29 11:14:50 +0200

    FMZ-CQRS-006 Add DatabaseSuperCommand for batch updates; implement UpdateBuilder and related tests

---
+++ src/test/groovy/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseSuperCommandSpec.groovy
@@ -0,0 +1,79 @@
+package com.filipmikolajzeglen.cqrs.hibernate.database
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.transaction.annotation.Transactional
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Specification
+
+@Sql
+@Testcontainers
+@Transactional
+@SpringBootTest
+class DatabaseSuperCommandSpec extends Specification {
+
+   @PersistenceContext
+   EntityManager entityManager
+
+   def "should update multiple entities using DatabaseSuperCommand.update"() {
+      when:
+      def query = DatabaseQuery.builder(DummyDatabaseEntity)
+            .property(DummyDatabaseEntity::getName).equalTo("1111111")
+            .build()
+      def command = DatabaseSuperCommand.update(DummyDatabaseEntity)
+            .set(DummyDatabaseEntity::setName, "Zmieniony")
+            .set(DummyDatabaseEntity::setFlag, false)
+            .where(query)
+      def updatedCount = command.execute(entityManager)
+      entityManager.flush()
+      entityManager.clear()
+
+      then:
+      updatedCount == 1
+      def changed = entityManager.createQuery(
+            "select e from DummyDatabaseEntity e where e.name = :name", DummyDatabaseEntity)
+            .setParameter("name", "Zmieniony")
+            .resultList
+      changed.size() == 1
+      changed.every { !it.flag }
+   }
+
+   def "should update by property name"() {
+      when:
+      def query = DatabaseQuery.builder(DummyDatabaseEntity)
+            .property("name").equalTo("2222222")
+            .build()
+      def command = DatabaseSuperCommand.update(DummyDatabaseEntity)
+            .set("number", 99L)
+            .where(query)
+      def updated = command.execute(entityManager)
+      entityManager.flush()
+      entityManager.clear()
+
+      then:
+      updated == 1
+      def found = entityManager.createQuery(
+            "select e from DummyDatabaseEntity e where e.name = :name", DummyDatabaseEntity)
+            .setParameter("name", "2222222")
+            .singleResult
+      found.number == 99L
+   }
+
+   def "should not update when where does not match"() {
+      when:
+      def query = DatabaseQuery.builder(DummyDatabaseEntity)
+            .property(DummyDatabaseEntity::getName).equalTo("Nieistnieje")
+            .build()
+      def command = DatabaseSuperCommand.update(DummyDatabaseEntity)
+            .set(DummyDatabaseEntity::setFlag, false)
+            .where(query)
+      def updated = command.execute(entityManager)
+      entityManager.flush()
+      entityManager.clear()
+
+      then:
+      updated == 0
+   }
+}
---
+++ src/test/resources/com/filipmikolajzeglen/cqrs/hibernate/database/DatabaseSuperCommandSpec.sql
@@ -0,0 +1,8 @@
+INSERT INTO fmzcqrshibernate.dummy_database_entity (id, name, flag, number)
+VALUES (-1, '1111111', TRUE, 1000),
+       (-2, '2222222', FALSE, 2000),
+       (-3, '3333333', FALSE, 3000),
+       (-4, '4444444', FALSE, 4000),
+       (-5, '5555555', TRUE, 5000),
+       (-6, '6666666', FALSE, 6000),
+       (-7, '7777777', TRUE, 7000);
